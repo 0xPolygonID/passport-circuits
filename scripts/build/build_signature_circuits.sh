12207fa9d557fb2d79e17bd928948402c152edd8
daveroga <daveroga@gmail.com>
2025-03-19 16:23:02 +0100

    add packages and contract verifiers for the different signature algorithms

--- scripts/build/build_signature_circuits.sh
+++ scripts/build/build_signature_circuits.sh
@@ -5,30 +5,38 @@ source "scripts/build/common.sh"
 # Circuit-specific configurations
 CIRCUIT_TYPE="signature"
 OUTPUT_DIR="build/${CIRCUIT_TYPE}"
+PACKAGE_DIR="package/${CIRCUIT_TYPE}"
 
 # Define circuits and their configurations
 # format: name:poweroftau:build_flag
 CIRCUITS=(
-    "signature_sha1_sha1_sha1_ecdsa_brainpoolP224r1:22:true"
+    # ECDSA circuits
+    "signature_sha1_sha1_sha1_ecdsa_brainpoolP224r1:22:false"
     # "signature_sha1_sha1_sha1_ecdsa_secp256r1:21:false" # Instance not found
+    "signature_sha224_sha224_sha224_ecdsa_brainpoolP224r1:22:false"
+    "signature_sha256_sha256_sha256_ecdsa_brainpoolP256r1:22:false"
+    "signature_sha256_sha256_sha256_ecdsa_brainpoolP384r1:23:false"
+    "signature_sha256_sha256_sha256_ecdsa_secp256r1:22:false"
+    "signature_sha256_sha256_sha256_ecdsa_secp384r1:23:false"
+    "signature_sha384_sha384_sha384_ecdsa_brainpoolP384r1:23:false"
+    "signature_sha384_sha384_sha384_ecdsa_brainpoolP512r1:24:true"
+    "signature_sha384_sha384_sha384_ecdsa_secp384r1:23:true"
+    "signature_sha512_sha512_sha512_ecdsa_brainpoolP512r1:24:false"
+    
+    # RSA circuits
+    "signature_sha1_sha1_sha1_rsa_65537_4096:20:false"
     # "signature_sha1_sha1_sha1_rsa_65537_2048:20:false" # Instance not found
     "signature_sha1_sha256_sha256_rsa_65537_4096:20:false"
-    "signature_sha224_sha224_sha224_ecdsa_brainpoolP224r1:22:false"
-    "signature_sha256_sha256_sha256_ecdsa_brainpoolP256r1:21:false"
-    "signature_sha256_sha256_sha256_ecdsa_secp256r1:21:false"
-    "signature_sha256_sha256_sha256_ecdsa_secp384r1:23:false"
-    "signature_sha256_sha256_sha256_rsa_65537_3072:20:false"
+    # "signature_sha256_sha256_sha256_rsa_65537_3072:20:false" # Instance not found
     "signature_sha256_sha256_sha256_rsa_65537_4096:20:false"
+    "signature_sha512_sha512_sha512_rsa_65537_4096:21:false"
+
+    # RSA-PSS circuits
     "signature_sha256_sha256_sha256_rsapss_3_32_2048:21:false"
     "signature_sha256_sha256_sha256_rsapss_65537_32_2048:21:false"
-    "signature_sha384_sha384_sha384_ecdsa_brainpoolP256r1:22:false"
-    "signature_sha384_sha384_sha384_ecdsa_brainpoolP384r1:22:false"
-    "signature_sha384_sha384_sha384_ecdsa_secp384r1:22:false"
-    "signature_sha512_sha512_sha512_ecdsa_brainpoolP256r1:22:false"
-    "signature_sha512_sha512_sha512_ecdsa_brainpoolP384r1:22:false"
-    "signature_sha512_sha512_sha512_ecdsa_brainpoolP512r1:24:false"
-    "signature_sha512_sha512_sha512_rsa_65537_4096:21:false"
+    "signature_sha256_sha256_sha256_rsapss_65537_32_3072:21:false"
+    "signature_sha384_sha384_sha384_rsapss_65537_48_2048:22:false"
     "signature_sha512_sha512_sha512_rsapss_65537_64_2048:22:false"
 )
 
-build_circuits "$CIRCUIT_TYPE" "$OUTPUT_DIR" "${CIRCUITS[@]}" 
+build_circuits "$CIRCUIT_TYPE" "$OUTPUT_DIR" "$PACKAGE_DIR" "${CIRCUITS[@]}" 
--- scripts/package/common.sh
+++ scripts/package/common.sh
@@ -12,13 +12,13 @@ package_circuit() {
     local CIRCUIT_NAME=$1
     local CIRCUIT_TYPE=$2
     local BUILD_DIR=$3
-    local OUTPUT_DIR=$4
+    local PACKAGE_DIR=$4
     local START_TIME=$(date +%s)
 
     echo -e "${BLUE}Generating witnesscalc package for circuit: $CIRCUIT_NAME${NC}"
     
     # Create output directory
-    mkdir -p ${OUTPUT_DIR}/${CIRCUIT_NAME}/
+    mkdir -p ${PACKAGE_DIR}/${CIRCUIT_NAME}/
     
     # Set circuit path based on CIRCUIT_TYPE
     local CIRCUIT_PATH
@@ -28,22 +28,38 @@ package_circuit() {
         CIRCUIT_PATH="circuits/${CIRCUIT_TYPE}/${CIRCUIT_NAME}.circom"
     fi
 
-    cp ${BUILD_DIR}/${CIRCUIT_NAME}/${CIRCUIT_NAME}.r1cs \
-        ${OUTPUT_DIR}/${CIRCUIT_NAME}/${CIRCUIT_NAME}.r1cs
+    if [ ! -f ${PACKAGE_DIR}/${CIRCUIT_NAME}/${CIRCUIT_NAME}.r1cs ]; then
+        echo -e "${YELLOW}Copying .r1cs file...${NC}"
+        cp ${BUILD_DIR}/${CIRCUIT_NAME}/${CIRCUIT_NAME}.r1cs \
+            ${PACKAGE_DIR}/${CIRCUIT_NAME}/${CIRCUIT_NAME}.r1cs
+        echo -e "${GREEN}Finished copying!${NC}"
+    else 
+        echo -e "${YELLOW}.r1cs file already copied${NC}"
+    fi
 
-    cp ${BUILD_DIR}/${CIRCUIT_NAME}/${CIRCUIT_NAME}_cpp/${CIRCUIT_NAME}.cpp \
-        ${OUTPUT_DIR}/${CIRCUIT_NAME}/${CIRCUIT_NAME}.cpp
+    if [ ! -f ${PACKAGE_DIR}/${CIRCUIT_NAME}/${CIRCUIT_NAME}.cpp ]; then
+        echo -e "${YELLOW}Copying ${CIRCUIT_NAME}.cpp file...${NC}"
+        cp ${BUILD_DIR}/${CIRCUIT_NAME}/${CIRCUIT_NAME}_cpp/${CIRCUIT_NAME}.cpp \
+            ${PACKAGE_DIR}/${CIRCUIT_NAME}/${CIRCUIT_NAME}.cpp
+        echo -e "${GREEN}Finished copying!${NC}"
+    else 
+        echo -e "${YELLOW}${CIRCUIT_NAME}.cpp file already copied${NC}"
+    fi
 
-    cp ${BUILD_DIR}/${CIRCUIT_NAME}/${CIRCUIT_NAME}_cpp/${CIRCUIT_NAME}.dat \
-        ${OUTPUT_DIR}/${CIRCUIT_NAME}/${CIRCUIT_NAME}.dat
-
-    echo -e "${BLUE}Copied ${CIRCUIT_NAME} build files${NC}"
+    if [ ! -f ${PACKAGE_DIR}/${CIRCUIT_NAME}/${CIRCUIT_NAME}.dat ]; then
+        echo -e "${YELLOW}Copying ${CIRCUIT_NAME}.dat file...${NC}"
+        cp ${BUILD_DIR}/${CIRCUIT_NAME}/${CIRCUIT_NAME}_cpp/${CIRCUIT_NAME}.dat \
+            ${PACKAGE_DIR}/${CIRCUIT_NAME}/${CIRCUIT_NAME}.dat
+        echo -e "${GREEN}Finished copying!${NC}"
+    else 
+        echo -e "${YELLOW}${CIRCUIT_NAME}.dat file already copied${NC}"
+    fi
 
     cd witnesscalc-template
     rm -rf build_witnesscalc
     ./build_gmp.sh host
     mkdir build_witnesscalc && cd build_witnesscalc
-    cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../package/${CIRCUIT_TYPE}/${CIRCUIT_NAME} -DCIRCUIT_FILE=../../${OUTPUT_DIR}/${CIRCUIT_NAME}/${CIRCUIT_NAME}.cpp
+    cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../package/${CIRCUIT_TYPE}/${CIRCUIT_NAME} -DCIRCUIT_FILE=../../${PACKAGE_DIR}/${CIRCUIT_NAME}/${CIRCUIT_NAME}.cpp
     make -j 8 && make install
     cd ../..
 }
@@ -52,7 +68,7 @@ package_circuits() {
     local CIRCUITS=("$@")
     local CIRCUIT_TYPE="$1"
     local BUILD_DIR="$2"
-    local OUTPUT_DIR="$3"
+    local PACKAGE_DIR="$3"
     shift 2 
     local TOTAL_START_TIME=$(date +%s)
 
@@ -62,7 +78,7 @@ package_circuits() {
         if [ "$PACKAGE_FLAG" = "true" ]; then
             # Package circuit
             echo -e "${BLUE}Packaging circuit $CIRCUIT_NAME${NC}"
-            package_circuit "$CIRCUIT_NAME" "$CIRCUIT_TYPE" "$BUILD_DIR" "$OUTPUT_DIR"
+            package_circuit "$CIRCUIT_NAME" "$CIRCUIT_TYPE" "$BUILD_DIR" "$PACKAGE_DIR"
         else
             echo -e "${GRAY}Skipping package for $CIRCUIT_NAME${NC}"
         fi
