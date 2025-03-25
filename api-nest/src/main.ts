30d75ff73cc6e2078bd9dcff45ef040aa741b73d
vbasiuk <volodymyr.basiuck@gmail.com>
2025-03-25 17:01:48 +0200

    remove api-nest (moved to separate repo)

--- api-nest/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.getHttpServer().setTimeout(300000);
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
--- api-nest/src/proof/proof.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProofController } from './proof.controller';
-import { ProofService } from './proof.service';
-
-@Module({
-  controllers: [ProofController],
-  providers: [ProofService],
-})
-export class ProofModule {}
