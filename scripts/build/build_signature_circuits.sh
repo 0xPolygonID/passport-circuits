3fe1a6f9df49324543006f62dd12553df2254743
vbasiuk <volodymyr.basiuck@gmail.com>
2025-04-01 14:47:54 +0300

    add missed instances

--- scripts/build/build_signature_circuits.sh
+++ scripts/build/build_signature_circuits.sh
@@ -12,8 +12,9 @@ PACKAGE_DIR="package/${CIRCUIT_TYPE}"
 CIRCUITS=(
     # ECDSA circuits
     "signature_sha1_sha1_sha1_ecdsa_brainpoolP224r1:22:false"
-    # "signature_sha1_sha1_sha1_ecdsa_secp256r1:21:false" # Instance not found
+    "signature_sha1_sha1_sha1_ecdsa_secp256r1:21:false"
     "signature_sha224_sha224_sha224_ecdsa_brainpoolP224r1:22:false"
+    "signature_sha256_sha224_sha224_ecdsa_secp224r1:22:false"
     "signature_sha256_sha256_sha256_ecdsa_brainpoolP256r1:22:false"
     "signature_sha256_sha256_sha256_ecdsa_brainpoolP384r1:23:false"
     "signature_sha256_sha256_sha256_ecdsa_secp256r1:22:false"
@@ -22,6 +23,7 @@ CIRCUITS=(
     "signature_sha384_sha384_sha384_ecdsa_brainpoolP512r1:24:false"
     "signature_sha384_sha384_sha384_ecdsa_secp384r1:23:false"
     "signature_sha512_sha512_sha512_ecdsa_brainpoolP512r1:24:false"
+    "signature_sha512_sha512_sha512_ecdsa_secp521r1:24:false"
     
     # RSA circuits
     "signature_sha1_sha1_sha1_rsa_65537_4096:20:false"
@@ -29,12 +31,15 @@ CIRCUITS=(
     "signature_sha1_sha256_sha256_rsa_65537_4096:20:false"
     # "signature_sha256_sha256_sha256_rsa_65537_3072:20:false" # Instance not found
     "signature_sha256_sha256_sha256_rsa_65537_4096:20:false"
+    "signature_sha256_sha256_sha256_rsa_3_4096:20:false"
+    "signature_sha512_sha512_sha256_rsa_65537_4096:21:false"
     "signature_sha512_sha512_sha512_rsa_65537_4096:21:false"
 
     # RSA-PSS circuits
     "signature_sha256_sha256_sha256_rsapss_3_32_2048:21:false"
     "signature_sha256_sha256_sha256_rsapss_65537_32_2048:21:false"
     "signature_sha256_sha256_sha256_rsapss_65537_32_3072:21:false"
+    "signature_sha256_sha256_sha256_rsapss_65537_64_2048:22:false"
     "signature_sha384_sha384_sha384_rsapss_65537_48_2048:22:false"
     "signature_sha512_sha512_sha512_rsapss_65537_64_2048:22:false"
 )
